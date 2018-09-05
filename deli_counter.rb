8ff5e1d8e0b81db75c39643fda334f10194496eb
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-09-05 19:47:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,5 +18,7 @@ end
 
 def now_serving(line)
   if line.length > 0
-    puts "Now serving #{line.shift}"
+    puts "Now serving #{line.shift}."
+  else 
+    puts "There is nobody waiting to be served!"
 end
