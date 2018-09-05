852cb8a02ff34d1c7236342ea9e540d3f8b125ea
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-09-05 19:39:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,7 @@ def line(array)
   if array.length > 0 
     array.each_with_index do |customer, index|
       customers << "#{index}. #{customer}"
-      
-    
+    end
   else 
     puts "The line is currently empty."
   
