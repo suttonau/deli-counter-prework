580a2267e58399d3da628c6ab2e8ad88d7a14708
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-09-05 19:49:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,10 +11,12 @@ def line(array)
   end 
 end
 
+
 def take_a_number(current_line, name)
   current_line << name
-  puts "Welcome, #{name}. You are #{current_line.length} in line."
+  puts "Welcome, #{name}. You are number #{current_line.length} in line."
 end 
+
 
 def now_serving(line)
   if line.length > 0
